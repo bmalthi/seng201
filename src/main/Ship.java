2d9f41aac75bbd6262a03600e0df0d1b9bfa0931
Kvie <75555021+kvieeee@users.noreply.github.com>
2021-05-09 11:51:21 +1200

    Update Ship.java

--- src/main/Ship.java
+++ src/main/Ship.java
@@ -1,13 +1,27 @@
 package main;
 import java.util.ArrayList;
 
+/**
+ * This class represents the four ships that the player can choose 
+ * at the beginning of the game
+ * 
+ * @author kvie
+ */
 public class Ship {
-	private String name;
-	private int numberOfCrew;
-	private String damageStatus;
+	private String name; 
+	private int numberOfCrew; 
+	private String damageStatus; 
 	private int repairCost;
-	private ArrayList<StorageList> storage;
+	private ArrayList<StorageList> storage; //ArrayList of storage that contain cargo
+	private ArrayList<Ship> ships; //ArrayList of ships that contain 4 ships
 	
+	/** 
+	 * 
+	 * @param name - name of the ship
+	 * @param numberOfCrew - number of crew on the ship
+	 * @param damageStatus - the ship's current damage status
+	 * @param repairCost - the cost to repair the ship's damage
+	 */
 	
 	public Ship(String name, int numberOfCrew, String damageStatus, int repairCost) {
 		this.name = name;
@@ -15,6 +29,7 @@ public class Ship {
 		this.damageStatus = damageStatus;
 		this.repairCost = repairCost;
 		this.storage = new ArrayList<StorageList>();
+		this.ships = new ArrayList<Ship>();
 		
 		this.storage.add(new StorageList("Cargo Hold 1", 10, ItemType.CARGO));
 		this.storage.add(new StorageList("Cannon Bay", 1, ItemType.WEAPON));
@@ -22,7 +37,30 @@ public class Ship {
 		
 	}
 	
+	public void createShip() {
+		/**
+		 * This method creates the four ships for the player to choose:
+		 * 
+		 * Ship: name, numberOfCrew, damageStatus, repairCost
+		 * 
+		 */
+		Ship ship1 = new Ship("Speedy Soul", 9, "10%", 180);
+		ships.add(ship1);
+		
+		Ship ship2 = new Ship("Sudden Storm", 10, "15%", 200);
+		ships.add(ship2);
+		
+		Ship ship3 = new Ship("Steel Skull", 11, "20%", 280);
+		ships.add(ship3);
+		
+		Ship ship4 = new Ship("Savage Sloop", 12, "25%", 300);
+		ships.add(ship4);
+	}
+	
 	public boolean hasSpace(Item item) {
+		/**
+		 * This method check if the storage has enough space for more items
+		 */
  		for (int i = 0; i < storage.size(); i++) {
  			if (storage.get(i).getType() == item.getType()) {
  				if (storage.get(i).remainingSpace() >= item.getSize()) {
@@ -36,6 +74,9 @@ public class Ship {
 
 
 	public void addItem(Item item) {
+		/**
+		 * This method adds item to the storage if the storage has space
+		 */
  		for (int i = 0; i < storage.size(); i++) {
  			if (storage.get(i).getType() == item.getType()) {
  				if (storage.get(i).remainingSpace() >= item.getSize()) {
@@ -46,6 +87,9 @@ public class Ship {
  	}
 
 	public boolean hasItem(Item item) {
+		/**
+		 * This method checks if the item already in the storage 
+		 */
  		for (int i = 0; i < storage.size(); i++) {
  			if (storage.get(i).getType() == item.getType()) {
  				if (storage.get(i).hasItem(item)) {
@@ -57,6 +101,9 @@ public class Ship {
  	}
 
 	public boolean removeItem(Item item) {
+		/**
+		 * This method removes item if it is already in the storage
+		 */
  		for (int i = 0; i < storage.size(); i++) {
  			if (storage.get(i).getType() == item.getType()) {
  				storage.get(i).removeItem(item);
@@ -66,27 +113,45 @@ public class Ship {
  	}	
 	
 	public int getCostPerDay() {
+		/**
+		 * This method get the cost per day
+		 */
 		return getNumberOfCrew();
 	}
 	
 	public String getName() {
+		/** 
+		 * This method gets the name of the ship
+		 */
 		return name;
 	}
 	
 	public int getNumberOfCrew() {
+		/**
+		 * This method gets the number of crew
+		 */
 		return numberOfCrew;
 	}
 
 	
 	public int repairCost() {
+		/**
+		 * This method gets the repair cost for the ship
+		 */
 		return repairCost;
 	}
 	
 	public String getdamageStatus() {
+		/**
+		 * This method gets the damage status of the ship
+		 */
 		return damageStatus;
 	}
 	
 	public String toString() {
+		/**
+		 * This method let the player know that the ship has been created successfully
+		 */
 		return ("Great choice! Now ship " + getName() + " is your ship" + "\n The ship has been damaged " + getdamageStatus() + " so you have to be careful while sailing");
 		
 	}
