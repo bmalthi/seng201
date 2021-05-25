6b571081e95c4359e45815b54a29463c2ca2d130
Kvie <75555021+kvieeee@users.noreply.github.com>
2021-05-25 14:05:03 +1200

    Update ViewIslandRoutes.java

--- src/ui/gui/ViewIslandRoutes.java
+++ src/ui/gui/ViewIslandRoutes.java
@@ -14,48 +14,42 @@ import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.JList;
 
-/**
- * This class represents the screen after the user clicked the "View Routes to This Island" button in View Island Properties Screen.
- * @author kvie
- *
- */
 public class ViewIslandRoutes extends Screen {
 
 	/**
 	 */
 	public ViewIslandRoutes(IslandTrader islandTrader) {
+		
 		super("View Routes To The Current Island", islandTrader);
 		
 	}
 
+//	/**
+//	 * This is only here because WindowBuilder needs a JFrame
+//	 * to be created within this file to allow us to edit the GUI
+//	 * 
+//	 * @wbp.parser.entryPoint
+//	 */
+//	protected void initialiseForWindowBuilder() {
+//		initialise(new JFrame());
+//	}
+	
 	/**
-   	* This is only here because WindowBuilder needs a JFrame
- 	 * to be created within this file to allow us to edit the GUI
- 	 * 
- 	 * @wbp.parser.entryPoint
- 	 */
- 	protected void initialiseForWindowBuilder() {
- 		initialise(new JFrame());
- 	}
- 
-	/**
-	 * Initialize the contents of the frame, which include:
-	 * a list of routes for the user to view
-	 * a "Back To Main Menu" button
+	 * Initialize the contents of the container.
 	 */
 	@Override
-	protected void initialise(final JFrame frame) {
+	protected void initialise(final JFrame container) {
 		List<Route> routes = getManager().getWorld().getRoutesFromCurrent();
-		frame.getContentPane().setBackground(new Color(47, 79, 79));
-		frame.getContentPane().setLayout(null);
+		container.getContentPane().setBackground(new Color(47, 79, 79));
+		container.getContentPane().setLayout(null);
 		
-		JTextArea lblHelloTrader = new JTextArea("Hello trader! Have you experienced some cool things in this island?\n\nHere are all routes avaiable to this island:");
-		lblHelloTrader.setLineWrap(true);
-		lblHelloTrader.setForeground(Color.WHITE);
-		lblHelloTrader.setFont(new Font("iCiel Brush Up", Font.PLAIN, 20));
-		lblHelloTrader.setBackground(new Color(47, 79, 79));
-		lblHelloTrader.setBounds(41, 48, 653, 104);
-		frame.getContentPane().add(lblHelloTrader);
+		JTextArea lblNewLabel_1_1 = new JTextArea("Hello trader! Have you experienced some cool things in this island?\n\nHere are all routes avaiable to this island:");
+		lblNewLabel_1_1.setLineWrap(true);
+		lblNewLabel_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1.setFont(new Font("iCiel Brush Up", Font.PLAIN, 20));
+		lblNewLabel_1_1.setBackground(new Color(47, 79, 79));
+		lblNewLabel_1_1.setBounds(41, 48, 653, 104);
+		container.getContentPane().add(lblNewLabel_1_1);
 		
 		// SHOULD BE USING TEXT AREA _ ASK TUTOR
 		// Create a ListModel to store the items in the JList
@@ -73,27 +67,22 @@ public class ViewIslandRoutes extends Screen {
 		routeList.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
 		routeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		routeList.setBounds(27, 219, 732, 115);
-		frame.getContentPane().add(routeList);
+		container.getContentPane().add(routeList);
 		
-		JButton btnBackToMenu = new JButton("Back to main menu");
-		btnBackToMenu.addActionListener(new ActionListener() {
+		JButton btnNewButton = new JButton("Back to main menu");
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				quit();
 				Screen screen = new MainScreen(islandTrader);
 				screen.show();
 			}
 		});
-		btnBackToMenu.setBounds(298, 428, 164, 75);
-		frame.getContentPane().add(btnBackToMenu);
+		btnNewButton.setBounds(298, 428, 164, 75);
+		container.getContentPane().add(btnNewButton);
 		
-//		JTextArea textArea = new JTextArea();
-//		textArea.setBounds(241, 371, 1, 16);
-//		frame.getContentPane().add(textArea);
-//		
-		
-		frame.setBackground(new Color(47, 79, 79));
-		frame.setBounds(100, 100, 785, 582);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		container.setBackground(new Color(47, 79, 79));
+		container.setBounds(100, 100, 785, 582);
+		container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
